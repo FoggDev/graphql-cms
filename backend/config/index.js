4f119f2284bbf68e2d6cc5c41229b45b775429ea
Carlos Santana <carlos.santana@dev.education>
2019-12-01 17:16:07 -0800

    V0 - Removing backend and moving frontend to root

--- backend/config/index.js
+++
@@ -1,7 +0,0 @@
-// Configuration
-import config from './config.json'
-
-// Configurations
-export const $db = () => config.db
-export const $security = () => config.security
-export const $serverPort = () => config.serverPort
--- backend/src/graphql/types/__tests__/index.test.js
+++
@@ -1,7 +0,0 @@
-import typeDefs from '../index'
-
-describe('#typeDefs', () => {
-  it('should match the GraphQL schema', () => {
-    expect(typeDefs).toMatchSnapshot()
-  })
-})
